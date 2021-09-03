fbb03f7938160f7ed2e04084154a7fc7d23c8e65
yogi0067 <yogesh.bansa0067@gmail.com>
2021-09-03 11:02:24 +0530

    test pull request from master

--- src/main/java/com/crm/qa/pages/DealsPage.java
+++ src/main/java/com/crm/qa/pages/DealsPage.java
@@ -3,5 +3,8 @@ package com.crm.qa.pages;
 import com.crm.qa.base.TestBase;
 
 public class DealsPage extends TestBase{
+	public static void main(String[] args) {
+		System.out.println("Test Pull Request");
+	}
 
 }
