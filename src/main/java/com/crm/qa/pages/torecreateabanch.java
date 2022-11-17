b2d7279cfa40d2985e276710bde33a5240a1eee4
gowth <gowth@DESKTOP-I5RNK02>
2022-11-17 15:45:06 +0530

    Creating a branch

---
+++ src/main/java/com/crm/qa/pages/torecreateabanch.java
@@ -0,0 +1,11 @@
+package com.crm.qa.pages;
+
+public class torecreateabanch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "update";
+		System.out.println(s);
+	}
+
+}
